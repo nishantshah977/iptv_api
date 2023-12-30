f7ea5422a9266a180fab6e70c436cd034bab23d5
Nishant Shah <99190466+nishantshah977@users.noreply.github.com>
2023-12-30 11:11:30 +0545

    Update index.js

--- index.js
+++ index.js
@@ -2,26 +2,33 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-axios.get('https://raw.githubusercontent.com/manikiptv/freecatv.github.io/main/freecatv.m3u8').then((response) =>{
- let list = response.data;
-  list = list.replace('#EXTM3U', '');
-list = list.replace(/#EXTINF:-1 ,/g, '');
-list = list.replace(/#EXTINF:-1 group-title="freecatv"/g, '');
+const start = async () => {
+  try {
+    const response = await axios.get('https://raw.githubusercontent.com/manikiptv/freecatv.github.io/main/freecatv.m3u8');
+    let list = response.data;
+    list = list.replace('#EXTM3U', '');
+    list = list.replace(/#EXTINF:-1 ,/g, '');
+    list = list.replace(/#EXTINF:-1 group-title="freecatv"/g, '');
 
-const splited_list = list.split('\n');
+    const splited_list = list.split('\n');
 
-const cleaned_list = [];
+    const cleaned_list = [];
 
-for (let i = 1; i < splited_list.length; i++) {
-  cleaned_list.push({ "channel": splited_list[i], "url": splited_list[i + 1] });
-  i += 1;
-}
+    for (let i = 1; i < splited_list.length; i++) {
+      cleaned_list.push({ "channel": splited_list[i], "url": splited_list[i + 1] });
+      i += 1;
+    }
 
-app.get('/', (req, res) => {
- res.setHeader('Content-Type', 'application/json');
-  res.json(cleaned_list);
-});
-})
+    app.get('/', async (req, res) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.json(cleaned_list);
+    });
+  } catch (error) {
+    console.error('Error fetching data:', error.message);
+  }
+};
+
+start();
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
